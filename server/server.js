bdca5cfcf9c115d9dc0e5f9f82b287476e0f883e
Ritesh Rai <riteshrai997@gmail.com>
2022-01-26 14:29:58 +0530

    server setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
-const express=require("express")
+const express = require("express");
+const userAuthApis = require("./routes/userControllers/userAuthApis");
+const connectDB = require("./db/mongoose");
 
-const app= express()
+// MongoDB Connection Fire
+connectDB();
 
-app.get("/",(req,res)=>{
- res.send("data send")
-})
+const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.listen(5000 || process.env.port,()=>{
-console.log("server running on 5000")
-})
+// root route for server
+app.get("/", (req, res) => {
+  res.send("Raddiwala Server Works!!");
+});
+
+// PORT for main server
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Listening on  http://localhost:${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+  });
+});
