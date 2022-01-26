f4e9a607c630e77a84b6dba9dbec61de435b24b8
Prathamesh Deshpande <prtdesh25@gmail.com>
2022-01-26 23:26:18 +0530

    about section minor changes

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -15,7 +15,7 @@ return(
                 Ever thought about the <span className = "text-change"  >scrap</span> at your home and what it can be <span className = "text-change"  >used</span> for?
                 Ever thought what can you do with it? 
                 We have a  <span className = "text-change"  >perfect solution.</span>
-                At <span className = "text-change"  >रaddiwala</span>, we Pick-up scrap from your home at your convenience and <span className = "text-change"  >pay</span> you the equivalent amount without any <span className = "text-change">extra charge.</span>
+                At <span className = "text-change"  >रaddiwala</span>, we Pick-up scrap from your home at your convenience and <span className = "text-change"  >pay</span> you the equivalent amount <span className = "text-change">without any extra charge.</span>
                 With your help and scrap, we can move a step to cleaner and <span className = "text-change">healthier environment</span> where recycling is part and parcel of our lives
             </p>
         </div>
