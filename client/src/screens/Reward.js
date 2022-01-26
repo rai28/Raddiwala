797cc9379fa5d4d886e6a2bb131831ce96b113b0
Prathamesh Deshpande <prtdesh25@gmail.com>
2022-01-26 14:47:45 +0530

    Reward Page implemented in React

---
+++ client/src/screens/Reward.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header2 from "../components/header2";
+import RewardComponent from "../components/reward";
+
+export default function Reward(){
+    return(
+        <div>
+            <Header2/>
+            <RewardComponent/>
+        </div>
+    )
+}
